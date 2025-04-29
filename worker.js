7ea43406e3c4a5184e57cc4d8f35a9b0dfc13cea
taddiemason <137760738+taddiemason@users.noreply.github.com>
2025-04-29 13:14:41 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
     try {
-      const response = await fetch("https://raw.githubusercontent.com/taddiemason/PersonalWebsite/main/index.html");
+      const response = await fetch("https://raw.githubusercontent.com/taddiemason/PersonalWebsite/main/Core.html");
 
       if (!response.ok) {
         return new Response("Failed to fetch HTML content.", { status: 500 });
